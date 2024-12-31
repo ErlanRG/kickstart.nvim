0c1d524b64294fd38f1797c84d52f42cb28e7d04
Erlan Rangel <erlanrangel@gmail.com>
2024-12-30 18:58:13 -0600

    add: missing keybinds and default plugins

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,13 +11,13 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>oo', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['<leader>oo'] = 'close_window',
         },
       },
     },
